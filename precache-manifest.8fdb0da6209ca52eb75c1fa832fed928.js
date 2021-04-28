d38fa45ee2fe910bce1fbead0197652427c59ba9
Pritam <www.pritamdas1999@gmail.com>
2021-04-28 16:52:58 +0530

    code

---
+++ precache-manifest.8fdb0da6209ca52eb75c1fa832fed928.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94744dbdd67b866213e3d8e3512200d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5af8aeda371703b55cc1",
+    "url": "/static/js/2.a35192b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a35192b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a375ec45660f911f8fff",
+    "url": "/static/js/main.a58aaadc.chunk.js"
+  },
+  {
+    "revision": "f5f8be9e6d9b9bdfc21e",
+    "url": "/static/js/runtime-main.c93a5cfa.js"
+  }
+]);
